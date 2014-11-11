7365e60ab8f46dd9f3faed774ba2ea33909c2c89
Ramon S. DeBelen <sakurazro@gmail.com>
2014-11-11 15:02:11 -0800

    'schema'

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,9 @@
 require File.expand_path('../boot', __FILE__)
-require "bootstrap-sass" 
 require 'rails/all'
+require "bootstrap-sass" 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module Omrails
   class Application < Rails::Application
